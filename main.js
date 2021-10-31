1e0d1f6edab326f7ee4c729bef2a4b723ba3cfcf
Camilo <cavs1010@gmail.com>
2021-11-01 07:01:47 +1000

    Adding  the generate Field function

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ const pathCharacter = '*';
 class Field {
 	constructor (pField){
 		this._pField = pField; //The field created
-		this._playerLocation = [0,0] // Initial position of the player
-		this._currentGameState = true;
+		this._currentGameState = true; //Initial state of the game
+		this._xPosition = 0; //Initial x position
+		this._yPosition = 0; //Initial y position
+		this._xSize = this._pField[0].length; //SIze of the field in the x axis
+		this._ySize = this._pField.length; //Size of the field in the y axis
 	}
 	get pField(){
 		return this._pField;
@@ -23,75 +26,87 @@ class Field {
 	set currentGameState(newState){
 		this._currentGameState = newState;
 	}
+
+	//Function that prints the current field
+	printField(field){
+		field.forEach(element=> console.log(element.join('')));
+	}
+
+	//Function that calculates the new position
+	calculatePosition (movement) {
+		switch(movement){
+			case 'u':
+				this._yPosition -= 1;
+				break;
+			case 'd':
+				this._yPosition += 1;
+				break;
+			case 'l':
+				this._xPosition -= 1;
+				break;
+			default:
+				this._xPosition += 1;
+		}
+	}
+
+	//Start the game
+	startGame(){
+		while(this._currentGameState){
+			try{
+				this._pField.forEach(element=> console.log(element.join('')));
+				var movement = prompt('Which way? ');
+				movement = movement.toLowerCase();
+				// Getting the direction to move
+				if (movement != 'u' && movement != 'd' && movement != 'l' && movement != 'r'){
+					throw Error(movement+ ' is not a valid input');
+				} else{
+						this._pField[this._yPosition][this._xPosition] = "*";
+						this.calculatePosition(movement);
+						if (this._xPosition>=this._xSize || this._xPosition<0 || this._yPosition>=this._ySize || this._yPosition<0){
+							console.log('the game is over');
+							this._currentGameState = false;
+						}else if(this._pField[this._yPosition][this._xPosition]=='O'){
+							console.log('You fell in the hole :(')
+							this._currentGameState = false;
+						}else if(this._pField[this._yPosition][this._xPosition]=='^'){
+							console.log('You find the Hat!!')
+							this._currentGameState = false;
+						}
+						else{
+							this._pField[this._yPosition][this._xPosition] = "\x1b[36m*\x1b[0m";
+						}
+					}
+				}
+			   catch(e){
+					this._currentGameState = false;
+				  console.log(e);
+				  }
+		  }
+	}
 }
 
 const myField = new Field([
-	['*', '░', 'O'],
+	["\x1b[36m*\x1b[0m", '░', 'O'],
 	['░', 'O', '░'],
 	['░', '^', '░'],
   ]);
 
+  //myField.startGame();
 
-  var currentPosition = [0,0];
-  var xPosition = currentPosition[0];
-  var yPosition = currentPosition[1];
-  var xSize =myField.pField[0].length;
-  var ySize = myField.pField.length;
-
-
-
-  while(myField.currentGameState){
-	try{
-		printField(myField._pField);
-		var movement = prompt('Which way? ');
-		movement = movement.toLowerCase();
-		// Getting the direction to move
-		if (movement != 'u' && movement != 'd' && movement != 'l' && movement != 'r'){
-			throw Error(movement+ 'is not a valid input');
-			myField.currentGameState = false;
-		} else{
-				calculatePosition(movement);
-				console.log('The current position in x is '+ xPosition + ' and in y is '+ yPosition);
-				if (xPosition>=xSize || xPosition<0 || yPosition>=ySize || yPosition<0){
-					console.log('the game is over');
-					myField.currentGameState = false;
-				}else if(myField._pField[yPosition][xPosition]=='O'){
-					console.log('You fell in the hole :(')
-					myField.currentGameState = false;
-				}else if(myField._pField[yPosition][xPosition]=='^'){
-					console.log('You find the Hat!!')
-					myField.currentGameState = false;
-				}
-				else{
-					myField._pField[yPosition][xPosition] = '*';
-				}
-			}
-		}
-	   catch(e){
-		  console.log('error');
-		  myField.currentGameState = false;
+  function generateField(width, height){
+	  x_position = Math.floor(Math.random() * width);
+	  y_position = Math.floor(Math.random() * height);
+	  let newField = []
+	  for (let i = 0; i < height; i++){
+		  newField[i] = [];
+		  for (let j = 0; j < width; j++){
+			newField[i][j] = fieldCharacter
 		  }
+	  }
+	  console.log('x position: '+x_position+' y position: '+ y_position)
+	  newField[y_position][x_position] = hat;
+	  newField.forEach(element=> console.log(element.join('')));
   }
- 
- 
-//Function that prints the current field
-function printField(field){
-	field.forEach(element=> console.log(element.join('')));
-}
 
-// Function that calculates the new position based on the movement
-function calculatePosition (movement) {
-	switch(movement){
-		case 'u':
-			yPosition -= 1;
-			break;
-		case 'd':
-			yPosition += 1;
-			break;
-		case 'l':
-			xPosition -= 1;
-			break;
-		default:
-			xPosition += 1;
-	}
-}
+  //Math.floor(Math.random() * max;
+  generateField(7,5);
