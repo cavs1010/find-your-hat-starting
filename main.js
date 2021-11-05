f4ba9c7a78529f49ae6aefce9a468999be58c813
cavs1010 <cavs1010@gmail.com>
2021-11-05 17:26:42 +1000

    final

--- main.js
+++ main.js
@@ -5,12 +5,40 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+// SUPPLEMENTARY FUNCTIONS---------------------------------------------------------
+// Function that creates the empty field
+function generateField(width, height){
+	let newField = [];
+	for (let i = 0; i < height; i++){
+		newField[i] = [];
+		for (let j = 0; j < width; j++){
+		  newField[i][j] = fieldCharacter
+		}
+	}
+	return newField;
+}
+
+// Function required for finding the array inide arrays
+Array.prototype.containsArray = function(val) {
+    var hash = {};
+    for(var i=0; i<this.length; i++) {
+        hash[this[i]] = i;
+    }
+    return hash.hasOwnProperty(val);
+}
+//-----------------------------------------------------------
+
+
+
+
+
 class Field {
-	constructor (pField){
+	constructor (pField, startPoint){
 		this._pField = pField; //The field created
 		this._currentGameState = true; //Initial state of the game
-		this._xPosition = 0; //Initial x position
-		this._yPosition = 0; //Initial y position
+		this._startPoint = startPoint;
+		this._xPosition = this._startPoint[1]; //Initial x position
+		this._yPosition = this._startPoint[0]; //Initial y position
 		this._xSize = this._pField[0].length; //SIze of the field in the x axis
 		this._ySize = this._pField.length; //Size of the field in the y axis
 	}
@@ -27,10 +55,6 @@ class Field {
 		this._currentGameState = newState;
 	}
 
-	//Function that prints the current field
-	printField(field){
-		field.forEach(element=> console.log(element.join('')));
-	}
 
 	//Function that calculates the new position
 	calculatePosition (movement) {
@@ -60,15 +84,15 @@ class Field {
 				if (movement != 'u' && movement != 'd' && movement != 'l' && movement != 'r'){
 					throw Error(movement+ ' is not a valid input');
 				} else{
-						this._pField[this._yPosition][this._xPosition] = "*";
+						this._pField[this._yPosition][this._xPosition] = pathCharacter;
 						this.calculatePosition(movement);
 						if (this._xPosition>=this._xSize || this._xPosition<0 || this._yPosition>=this._ySize || this._yPosition<0){
 							console.log('the game is over');
 							this._currentGameState = false;
-						}else if(this._pField[this._yPosition][this._xPosition]=='O'){
+						}else if(this._pField[this._yPosition][this._xPosition] == hole){
 							console.log('You fell in the hole :(')
 							this._currentGameState = false;
-						}else if(this._pField[this._yPosition][this._xPosition]=='^'){
+						}else if(this._pField[this._yPosition][this._xPosition]== hat){
 							console.log('You find the Hat!!')
 							this._currentGameState = false;
 						}
@@ -83,66 +107,44 @@ class Field {
 				  }
 		  }
 	}
-}
 
-const myField = new Field([
-	["\x1b[36m*\x1b[0m", '░', 'O'],
-	['░', 'O', '░'],
-	['░', '^', '░'],
-  ]);
-myField.startGame();
-  
-  // Creation of the initial field
-  let newField = []
-  function generateField(width, height){
-	
-	  for (let i = 0; i < height; i++){
-		  newField[i] = [];
-		  for (let j = 0; j < width; j++){
-			newField[i][j] = fieldCharacter
-		  }
-	  }
-  }
+	// Creating a field
+	static createField(width, height, required_positions){
+		var look_random = true //Keep looking for random values
+		var positions_secured = []; //Array of positions already used
 
-
-var width = 4 //Width of the field
-var height = 4 //Height of the field
-var required_positions = 5 //Number of required position in the field
-var look_random = true //Keep looking for random values
-var positions_secured = []; //Array of positions already used
-
-
-// Function required for finding the array inide arrays
-Array.prototype.containsArray = function(val) {
-    var hash = {};
-    for(var i=0; i<this.length; i++) {
-        hash[this[i]] = i;
-    }
-    return hash.hasOwnProperty(val);
-}
-
-//Creation of the fields
-while (look_random)
-  	if (positions_secured.length<required_positions){
-		var ran_x_pos = Math.floor(Math.random() * width);
-		var ran_y_pos = Math.floor(Math.random() * height);
-		var single_position = []; //Single position to create
-		single_position.push(ran_x_pos);
-		single_position.push(ran_y_pos);
-		if(!positions_secured.containsArray(single_position)){
-			positions_secured.push(single_position);
+		while (look_random)
+  			if (positions_secured.length<required_positions){
+				var ran_x_pos = Math.floor(Math.random() * height);
+				var ran_y_pos = Math.floor(Math.random() * width);
+				var single_position = []; //Single position to create
+				single_position.push(ran_x_pos);
+				single_position.push(ran_y_pos);
+				if(!positions_secured.containsArray(single_position)){
+					positions_secured.push(single_position);
+				}
+	  		} 
+		else{
+			look_random = false;
+	 	};
+		var newField = generateField(width, height);
+		newField[positions_secured[0][0]][positions_secured[0][1]] = '\x1b[36m*\x1b[0m';
+		newField[positions_secured[1][0]][positions_secured[1][1]] = hat;
+		for (let i = 2; i<required_positions; i++){
+			newField[positions_secured[i][0]][positions_secured[i][1]] = hole;
+			}
+		//newField.forEach(element=> console.log(element.join('')));	
+		return [newField,[positions_secured[0][0],positions_secured[0][1]]];	
 		}
-	  } 
-	else{
-		look_random = false;
-	  };
-
-generateField(4,4);
-
-newField[positions_secured[0][0]][positions_secured[0][1]] = '\x1b[36m*\x1b[0m';
-newField[positions_secured[1][0]][positions_secured[1][1]] = '^';
-for (let i = 2; i<required_positions; i++){
-	newField[positions_secured[i][0]][positions_secured[i][1]] = 'O';
+	
 }
 
-//newField.forEach(element=> console.log(element.join('')));
+
+const [newField3, startingPoint3] = Field.createField(10,7,6)
+const myfinalField = new Field(newField3, startingPoint3);
+myfinalField.startGame();
+
+
+
+
+
